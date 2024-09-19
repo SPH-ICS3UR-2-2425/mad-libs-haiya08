f4c41a4ccaf02feae55d9f6c0477efca3d7c447b
haiya08 <haiya.agiel@stu.ocsb.ca>
2024-09-19 13:20:30 -0400

    almost done

--- src/Main.java
+++ src/Main.java
@@ -16,10 +16,7 @@ public class Main {
 		
 		Scanner in = new Scanner(System.in);
 		System.out.println("Welcome to Mad Libs!");
-		System.out.println("What's your name?");
-		String name = in.nextLine();
-		System.out.println("One day, "+name+" wrote a Mad Lib in class!");
-		
+				
 		int num1 = in.nextInt();
 		in.nextLine();
 		System.out.println("Enter a name: ");
@@ -33,26 +30,23 @@ public class Main {
 		System.out.println("Enter a decimal number above 10: ");
 		double decNumber = in.nextDouble();
 		in.nextLine();
-		System.out.println("Enter an outdoor location: ");
+		System.out.println("Enter an outdoor location that starts with 'the': ");
 		String location = in.nextLine();
-		System.out.println("Enter a high number: ");
-		int score = in.nextInt();
 		System.out.println("Enter an emotion ending in ing: ");
 		String emotion = in.nextLine();
 		System.out.println("Enter a number above 100: ");
 		int score2 = in.nextInt();
 		System.out.println("Pick a number 1 through 5");
 		int score3 = in.nextInt();
-		
-		
-		
-	
-		
+		System.out.println("Pick another number 50-100 ");
+		int score4 = in.nextInt();
 		
 		//story
-		System.out.println ("Yesterday "+person1+" and "+person2+" were selling "+drink+" for "+score3+"" . After selling "+drink+" for  at "
-			+" "+location+" for "+decNumber+" of minutes, a "+animal+" attacked and stole amount of money. "
-			+ "person 1 and 2 started crying until a stranger decided to give them amount of money "); 
-	}
+
+		System.out.println ("Yesterday "+person1+" and "+person2+" were selling "+drink+" for "+score3+" dollars at "+location+" . "
+				+ "After selling "+drink+" at "+location+" for "+decNumber+" minutes, a "+animal+""
+				+ " attacked them and stole "+score4+" dollars from them . "
+				+ ""+person1+" and "+person2+" started "+emotion+" until a good samaritan saw what happend and decided "
+				+ "to give them "+score2+" dollars"); 
 
 }
