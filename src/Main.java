1f1e7712980e538bb8904568ac724c23be1b83d7
haiya08 <haiya.agiel@stu.ocsb.ca>
2024-09-23 13:17:27 -0400

    finished product

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,19 @@
 import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
 		/*
-		 * MadLibs Program
-		 * Author: 
-		 * Class: ICS3U
-		 * Date: Sept. 16, 2024
-		 * Make a program that asks for 10 inputs
-		 * reads them into variables, and then prints out a story
+		 * MadLibs Program Author: Class: ICS3U Date: Sept. 16, 2024 Make a program that
+		 * asks for 10 inputs reads them into variables, and then prints out a story
 		 * Make sure to include Strings, ints, and doubles!
 		 */
-		
-		//number to a string not so good
-		
+
+		// number to a string not so good
+
 		Scanner in = new Scanner(System.in);
 		System.out.println("Welcome to Mad Libs!");
-				
+
 		int num1 = in.nextInt();
 		in.nextLine();
 		System.out.println("Enter a name: ");
@@ -40,13 +37,14 @@ public class Main {
 		int score3 = in.nextInt();
 		System.out.println("Pick another number 50-100 ");
 		int score4 = in.nextInt();
-		
-		//story
 
-		System.out.println ("Yesterday "+person1+" and "+person2+" were selling "+drink+" for "+score3+" dollars at "+location+" . "
-				+ "After selling "+drink+" at "+location+" for "+decNumber+" minutes, a "+animal+""
-				+ " attacked them and stole "+score4+" dollars from them . "
-				+ ""+person1+" and "+person2+" started "+emotion+" until a good samaritan saw what happend and decided "
-				+ "to give them "+score2+" dollars"); 
+		// story
 
+		System.out.println("Yesterday " + person1 + " and " + person2 + " were selling " + drink + " for " + score3
+				+ " dollars at " + location + " . " + "After selling " + drink + " at " + location + " for " + decNumber
+				+ " minutes, a " + animal + "" + " attacked them and stole " + score4 + " dollars from them . " + ""
+				+ person1 + " and " + person2 + " started " + emotion
+				+ " until a good samaritan saw what happend and decided " + "to give them " + score2 + " dollars");
+
+	}
 }
